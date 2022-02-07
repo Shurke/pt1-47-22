074e7e38912004c92ad39499596a8d5f7448edaa
Jenderlion <vladchesyan@gmail.com>
2022-02-07 15:07:34 +0300

    Add homework 1 (change in task 1)

--- src/homework1/task1.py
+++ src/homework1/task1.py
@@ -7,10 +7,23 @@ Output: Общая цена 9 рублей 60 копеек
 """
 
 
-print('Введите стоимость:')
-M = float(input('Рублей: '))
-N = float(input('Копеек: '))
-S = float(input('Введите кол-во: '))
+str_1 = input('Введите стоимость в удобном для вас формате (сначала рубли, потом копейки,а количество в конце):')
+num_str = '0 1 2 3 4 5 6 7 8 9'
+str_2 = ''
+numbers = []
+
+for char in list(str_1):
+    if char in num_str:
+        str_2 += char
+    else:
+        str_2 += ' '
+
+for num in str_2.split(' '):
+    numbers.append(num)
+
+M = float(numbers[0])
+N = float(numbers[1])
+S = float(numbers[2])
 
 part_price = int(N*S % 100)
 full_price = int(M*S + N*S//100)

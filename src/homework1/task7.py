0c7d42a14e11f6663bdc710928dacf6be6a05ffa
SidorovVlad <rusherblr@gmail.com>
2022-02-08 11:03:59 +0300

    homework1

---
+++ src/homework1/task7.py
@@ -0,0 +1,17 @@
+"""Даны: три стороны треугольника. Требуется: проверить, действительно ли это стороны треугольника.
+Если стороны определяют треугольник, найти его площадь. Если нет, вывести сообщение о неверных данных."""
+
+
+a = int(input("Enter the length of the 1st side "))
+b = int(input("Enter the length of the 2nd side "))
+z = int(input("Enter the length of the 3rd side "))
+
+if a + b > z and a + z > b and b + z > a:
+    print("This is triangle")
+    #Heron's formula
+    p = 0.5*(a+b+z)
+    s = (p*(p-a)*(p-b)*(p-z))**0.5
+    s = round(s, 2)
+    print("area of a triangle", s)
+else:
+    print("Your data is not correct")

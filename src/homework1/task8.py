e22e3ba4f0b4e47aa40bb26ee600005386aa4b4d
Kissloroid <kissloroid@gmail.com>
2022-02-08 14:11:26 +0300

    HM1 task8.2

--- src/homework1/task8.py
+++ src/homework1/task8.py
@@ -18,3 +18,27 @@ def reverse_words(text):
 
 string = input('Введите строку: ')
 print(reverse_words(string))
+
+
+"""
+Given an array of integers your solution should find the smallest integer.
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+"""
+
+
+def find_smallest_int(arr):
+    smallest = arr[0]
+    for i in range(0, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+    return smallest
+
+
+string = input('Введите числа через пробел: ')
+x = string.split(' ')
+array = [int(elem) for elem in x]
+print('Наименьшее число: ' + str(find_smallest_int(array)))

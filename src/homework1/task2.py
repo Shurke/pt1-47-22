b3a2ad7a499a1a3338e5046b024bc2f9d3a59181
Jenderlion <vladchesyan@gmail.com>
2022-02-08 22:04:44 +0300

    Change all files to 100 character strings; set changes in task 1-2

--- src/homework1/task2.py
+++ src/homework1/task2.py
@@ -1,27 +1,29 @@
 """
-2. Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания.
+2. Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки
+препинания.
 Подсказки:
 - my_string.split([chars]) возвращает список строк.
 - len(list) - количество элементов в списке
 """
 
 
+import string
+
 str_1 = (input('Исходная строка: '))
-marks = r'!?.,()[]{}\/:;—'
 str_2 = ''
 best_word = ''
-best_word_lenth = 0
+best_word_length = 0
 
 for char in str_1:
-    if char in marks:
-        continue
-    else:
+    if char not in string.punctuation:
         str_2 += char
+    else:
+        str_2 += ' '
 
 list_s = str_2.split()
 for small_str in list_s:
-    if len(small_str) > best_word_lenth:
+    if len(small_str) > best_word_length:
         best_word = small_str
-        best_word_lenth = len(small_str)
+        best_word_length = len(small_str)
 
-print(f'Самое длинное слово - это "{best_word}" с количеством букв, равным {best_word_lenth}')
+print(f'Самое длинное слово - это "{best_word}" с количеством букв, равным {best_word_length}')
--- src/homework1/task4.py
+++ src/homework1/task4.py
@@ -1,17 +1,19 @@
 """
-4. Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке. Учитывать только английские
-буквы.
+4. Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке.
+Учитывать только английские буквы.
 """
 
+
+import string
 
 str_1 = input('Исходная строка: ')
 big_count = 0
 small_count = 0
 
 for char in str_1:
-    if char.isupper():
+    if char in string.ascii_uppercase:
         big_count += 1
-    elif char.islower():
+    elif char in string.ascii_lowercase:
         small_count += 1
 
-print(f'Заглавных букв {big_count}, а строчных - {small_count}!')
+print(f'Заглавных английских букв {big_count}, а строчных - {small_count}!')

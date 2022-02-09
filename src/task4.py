94ba34edd0c9bbb67ba7df10624e155557a0aa22
Kir3103 <strunets96@gmail.com>
2022-02-09 12:38:33 +0300

    true_variant

--- src/task4.py
+++ src/task4.py
@@ -2,4 +2,14 @@
 Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке.
 Учитывать только английские буквы
 """
-fghgh
+
+str_1 = input('Введите строку:')
+sum_small = 0
+sum_big = 0
+for word in str_1:
+    if 'a' <= word <= 'z':
+        sum_small = sum_small + 1
+    elif 'A' <= word <= 'Z':
+        sum_big = sum_big + 1
+print('Сумма строчных букв =', sum_small)
+print('Сумма прописных букв =', sum_big)

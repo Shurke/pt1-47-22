5ac027568b281bcc0049ce3bc86d06a477a235e2
DmitryBakacheuski <98980557+DmitryBakacheuski@users.noreply.github.com>
2022-02-10 21:29:39 +0300

    Fixed

--- src/homework1/task2.py
+++ src/homework1/task2.py
@@ -1,11 +1,14 @@
-'''Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания.
+'''Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки
+препинания.
 '''
+
 import string
-a =input().translate(str.maketrans('','',string.punctuation)).split(' ')
+
+a = input().translate(str.maketrans('', '', string.punctuation)).split(' ')
 b = 0
 c = None
 for i in a:
     if len(i) >= b:
         b += len(i)
         c = i
-print (c)
+print(c)

93d2b8d3a7f7f6735ef9076f96a308a5d3a0556d
andrejmoroz89 <andrejmoroz89@mail.ru>
2022-02-10 18:05:31 +0300

    fixed whitespace around arithmetic operator

--- src/homework1/task7.py
+++ src/homework1/task7.py
@@ -9,7 +9,7 @@ b = float(input('b = '))
 c = float(input('c = '))
 
 if a + b > c and a + c > b and b + c > a:
-    p = (a + b + c)/2
+    p = (a + b + c) / 2
     s = math.sqrt(p * (p - a) * (p - b) * (p - c))
     print(s)
 else:

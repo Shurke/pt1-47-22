6a9780d53c67e114eaa0f08f9f7924ab397caf1a
andrejmoroz89 <andrejmoroz89@mail.ru>
2022-02-10 17:58:13 +0300

    init commit

---
+++ src/homework1/task3.py
@@ -0,0 +1,12 @@
+#  3. Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы. Например, если было введено
+#  "abc cde def", то должно быть выведено "abcdef"
+
+
+text = input('Введите текст: ')
+newText = ''
+
+for i in text:
+    if i not in newText and i != ' ':
+        newText += i
+        
+print(newText)

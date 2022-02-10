8e4efd78d8b2add5188a88457226cca8b74e21f1
Jenderlion <vladchesyan@gmail.com>
2022-02-10 22:21:28 +0300

    Add task3

---
+++ src/homework2/task3.py
@@ -0,0 +1,18 @@
+"""
+Tuple practice
+Создайте кортеж из одного элемента, чтобы при итерировании по этому элементу последовательно
+выводились значения 1, 2, 3. Убедитесь что len() исходного кортежа возвращает 1.
+"""
+
+
+TUPLE = str(123),
+for CHAR in TUPLE[0]:
+    print(CHAR)
+print(len(TUPLE))
+
+#OR
+
+TUPLE = [1, 2, 3],
+for CHAR in TUPLE[0]:
+    print(CHAR)
+print(len(TUPLE))

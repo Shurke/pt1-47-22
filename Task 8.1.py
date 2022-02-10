f9386ffd5bddbe3237ca81bdef4ec5ae17af1af8
PankPanda <54017588+PankPanda@users.noreply.github.com>
2022-02-10 13:55:59 +0300

    Add files via upload
    CodeWars stuff

---
+++ Task 8.1.py
@@ -0,0 +1,13 @@
+# For every good kata idea there seem to be quite a few bad ones!
+
+# In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one
+# or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good
+# ideas, as is often the case, return 'Fail!'.
+
+def well(x):
+    if x.count("good") == 0:
+        return "Fail!"
+    elif x.count("good") <= 2:
+        return "Publish!"
+    else:
+        return "I smell a series!"

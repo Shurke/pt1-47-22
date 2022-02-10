776416e7b770473aa64fabe8ed6f19b620f1e83d
Kissloroid <kissloroid@gmail.com>
2022-02-10 14:36:33 +0300

    HM1 task8.5

--- src/homework1/task8.py
+++ src/homework1/task8.py
@@ -14,16 +14,16 @@ Examples:
 """
 
 
-# def reverse_words(text):
-#     rev = []
-#     lst = text.split(' ')
-#     for word in lst:
-#         rev.append(word[::-1])
-#     return ' '.join(rev)
-#
-#
-# string = input('Введите строку: ')
-# print(reverse_words(string))
+def reverse_words(text):
+    rev = []
+    lst = text.split(' ')
+    for word in lst:
+        rev.append(word[::-1])
+    return ' '.join(rev)
+
+
+string = input('Введите строку: ')
+print(reverse_words(string))
 
 
 """
@@ -36,18 +36,18 @@ You can assume, for the purpose of this kata, that the supplied array will not b
 """
 
 
-# def find_smallest_int(arr):
-#     smallest = arr[0]
-#     for i in range(0, len(arr)):
-#         if arr[i] < smallest:
-#             smallest = arr[i]
-#     return smallest
-#
-#
-# string = input('Введите числа через пробел: ')
-# x = string.split(' ')
-# array = [int(elem) for elem in x]
-# print('Наименьшее число: ' + str(find_smallest_int(array)))
+def find_smallest_int(arr):
+    smallest = arr[0]
+    for i in range(0, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+    return smallest
+
+
+string = input('Введите числа через пробел: ')
+x = string.split(' ')
+array = [int(elem) for elem in x]
+print('Наименьшее число: ' + str(find_smallest_int(array)))
 
 
 """
@@ -80,3 +80,21 @@ https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/python
 def make_upper_case(s):
     upper = s.upper()
     return upper
+
+
+
+"""
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/python
+"""
+
+
+def check(seq, elem):
+    for i in seq:
+        if i == elem:
+            return True
+    return False

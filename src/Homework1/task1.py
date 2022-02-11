64249fbb34efaf8b21259a5348c79ebbae4ff400
RusherBLR <rusherblr@gmail.com>
2022-02-11 18:56:55 +0300

    task1 flake8

--- src/Homework1/task1.py
+++ src/Homework1/task1.py
@@ -1,5 +1,5 @@
-""" Напишите программу, которая считает общую цену. Вводится M рублей и N копеек цена, а также количество S товара
-Посчитайте общую цену в рублях и копейках за L товаров.
+""" Напишите программу, которая считает общую цену. Вводится M рублей и N копеек цена,
+а также количество S товара. Посчитайте общую цену в рублях и копейках за L товаров.
 Пример:
 Input: Цена одной вещи 3 рубля 20 копеек, посчитать 3 предмета.
 Output: Общая цена 9 рублей 60 копеек
@@ -11,6 +11,6 @@ N = int(input("Enter the quantity of kopecks "))
 print("item price", M, "rubles", N, "kopecks ")
 S = int(input("Enter the quantity of item "))
 print("quantity of item ", S)
-L = M*S + N*S*0.01
+L = M * S + N * S * 0.01
 x = int(L)
-print("total price", x, "rubles", int((L-x)*100), "kopecks")
+print("total price", x, "rubles", int((L - x) * 100), "kopecks")

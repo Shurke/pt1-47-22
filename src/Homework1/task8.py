821dbb5ea6292d4fe54d3bad69e60a21171180b6
RusherBLR <rusherblr@gmail.com>
2022-02-11 18:48:49 +0300

    task8 new

--- src/Homework1/task8.py
+++ src/Homework1/task8.py
@@ -1,3 +1,5 @@
+from typing import List, Any
+
 """def is_even(num: int) -> bool:
     # your code here
     return False
@@ -13,27 +15,25 @@ if __name__ == '__main__':
 
 def is_even(num: int) -> bool:
     # your code here
-    return num/2 == num//2
+    return num / 2 == num // 2
+
+
 if __name__ == '__main__':
     print("Example:")
     print(is_even(2))
     # These "asserts" are used for self-checking and not for an auto-testing
-    assert is_even(2) == True
-    assert is_even(5) == False
-    assert is_even(0) == True
+    assert is_even(2) is True
+    assert is_even(5) is False
+    assert is_even(0) is True
     print("Coding complete? Click 'Check' to earn cool rewards!")
 
-
 """def between_markers(text: str, begin: str, end: str) -> str:
-
        # returns substring between two given markers
-
     # your code here
     return ''
 if __name__ == '__main__':
     print('Example:')
     print(between_markers('What is >apple<', '>', '<'))
-
     # These "asserts" are used for self-checking and not for testing
     assert between_markers('What is >apple<', '>', '<') == "apple"
     assert between_markers('What is [apple]', '[', ']') == "apple"
@@ -49,7 +49,9 @@ def between_markers(text: str, begin: str, end: str) -> str:
     # your code here
     a = text.find(begin)
     b = text.find(end)
-    return text[a+1:b]
+    return text[a + 1:b]
+
+
 if __name__ == '__main__':
     print('Example:')
     print(between_markers('What is >apple<', '>', '<'))
@@ -59,7 +61,6 @@ if __name__ == '__main__':
     assert between_markers('What is ><', '>', '<') == ""
     assert between_markers('>apple<', '>', '<') == "apple"
     print('Wow, you are doing pretty good. Time to check it!')
-
 
 """def is_acceptable_password(password: str) -> bool:
     # your code here
@@ -78,16 +79,17 @@ if __name__ == '__main__':
 def is_acceptable_password(password: str) -> bool:
     # your code here
     return len(password) > 6
+
+
 if __name__ == '__main__':
     print("Example:")
     print(is_acceptable_password('short'))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    assert is_acceptable_password('short') == False
-    assert is_acceptable_password('muchlonger') == True
-    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('short') is False
+    assert is_acceptable_password('muchlonger') is True
+    assert is_acceptable_password('ashort') is False
     print("Coding complete? Click 'Check' to earn cool rewards!")
-
 
 """from typing import List, Any
 def all_the_same(elements: List[Any]) -> bool:
@@ -109,25 +111,25 @@ if __name__ == '__main__':
     print("Coding complete? Click 'Check' to earn cool rewards!")"""
 
 
-from typing import List, Any
 def all_the_same(elements: List[Any]) -> bool:
-    if elements == []:
+    if not elements:
         return True
     y = elements.count(elements[0])
     if y == len(elements):
         return True
     return False
+
+
 if __name__ == '__main__':
     print("Example:")
     print(all_the_same([1, 1, 1]))
     # These "asserts" are used for self-checking and not for an auto-testing
-    assert all_the_same([1, 1, 1]) == True
-    assert all_the_same([1, 2, 1]) == False
-    assert all_the_same(['a', 'a', 'a']) == True
-    assert all_the_same([]) == True
-    assert all_the_same([1]) == True
+    assert all_the_same([1, 1, 1]) is True
+    assert all_the_same([1, 2, 1]) is False
+    assert all_the_same(['a', 'a', 'a']) is True
+    assert all_the_same([]) is True
+    assert all_the_same([1]) is True
     print("Coding complete? Click 'Check' to earn cool rewards!")
-
 
 """def first_word(text: str) -> str:
     returns the first word in a given text.
@@ -167,6 +169,8 @@ def first_word(text: str) -> str:
     """
     text = text.split()
     return text[0]
+
+
 if __name__ == "__main__":
     print("Example:")
     print(first_word("Hello world"))

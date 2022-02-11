95b62293a62a947383325e081a0fb174ec5d776c
RusherBLR <rusherblr@gmail.com>
2022-02-11 20:13:11 +0300

    Task4 Fix

--- src/Homework1/task4.py
+++ src/Homework1/task4.py
@@ -1,16 +1,16 @@
 """ Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке.
 Учитывать только английские буквы."""
 
-
+import string
 message = input("enter your message ")
 message_low = 0
 message_high = 0
 high = "QWERTYUIOPASDFGHJKLZXCVBNM"
 low = "qwertyuiopasdfghjklzxcvbnm"
 for letter in message:
-    if letter in high:
+    if letter in string.ascii_uppercase:
         message_high += 1
-    elif letter in low:
+    elif letter in string.ascii_lowercase:
         message_low += 1
 print("Your massage: ", message)
 print(message_low, "lowercase letters in your massage")

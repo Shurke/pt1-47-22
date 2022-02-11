a1de7b4f3d3d7fbcaeecabf47ce67e4e931ef0cb
RusherBLR <rusherblr@gmail.com>
2022-02-11 19:48:06 +0300

    task 5 fix

--- src/Homework1/task5.py
+++ src/Homework1/task5.py
@@ -7,14 +7,14 @@ n = input("Enter the sequence number of Fibonacci (n) = ")
 n = int(n)
 i = 0
 if n > 0:
-    while i < n - 1:
+    while i < n - 2:
         num_sum = num1 + num2
         num1 = num2
         num2 = num_sum
         i = i + 1
     print("For n =", n, "Fn = ", num2)
 elif n < 0:
-    while i > n + 1:
+    while i > n + 2:
         num_sum = num1 - num2
         num1 = num2
         num2 = num_sum

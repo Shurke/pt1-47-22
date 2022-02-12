7479beaf17a4e2816fe4e9b7803f3be8423ae3ad
So4ik <sachishin.aleks@bk.ru>
2022-02-12 14:06:43 +0200

    a;;djfpasokgjpaogjag

--- src/task8.py
+++ src/task8.py
@@ -52,7 +52,7 @@ else:
 
 """
 Given an integer,perform the following conditional actions: If n is odd, print Weird
-If n is even and in the inclusive range of 2 to 5, print Not Weird If n  is even and in the 
+If n is even and in the inclusive range of 2 to 5, print Not Weird If n is even and in the
 inclusive range of 6 to 20, print Weird If n is even and greater than 20 , print - NotWeird"""
 
 n = int(input("ВВедите число"))

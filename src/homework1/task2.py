286bacfb0f1f6baf20b2dc5f105a53721a0b2925
Oleg Galushko <55463091+inorangestylee@users.noreply.github.com>
2022-02-13 19:47:30 +0300

    fix: task2.py

--- src/homework1/task2.py
+++ src/homework1/task2.py
@@ -7,7 +7,7 @@
 import string
 
 
-def get_longest_word(s, allowHyphen=False):
+def get_longest_word(s, allow_hyphen=False):
     """Возвращает первое самое длинное слово в строке и его длину
 
     Существуют слова которые пишутся через дефис, например:
@@ -16,27 +16,28 @@ def get_longest_word(s, allowHyphen=False):
     Чтобы этого избежать реализован аргумент allowHyphen
 
     :param str s: предложение или текст
-    :param bool allowHyphen: (Default False) количество товаров
+    :param bool allow_hyphen: (Default False) количество товаров
     :returns: кортеж (слово, длина)
     :rtype: tuple
     """
     longest_word = ''
     longest_word_length = 0
     punctuation = string.punctuation
-    if allowHyphen:
+    if allow_hyphen:
         punctuation = punctuation.replace('-', '')
     cleared_s = "".join(ch for ch in s if ch not in punctuation)
     for w in cleared_s.split():
-        if len(w) > longest_word_length:
-            longest_word_length = len(w)
+        word_length = len(w)
+        if word_length > longest_word_length:
+            longest_word_length = word_length
             longest_word = w
     return (longest_word, longest_word_length)
 
 
 input_str = input("Введите строку: ")
 
-r1 = get_longest_word(input_str)
-r2 = get_longest_word(input_str, allowHyphen=True)
+result1 = get_longest_word(input_str)
+result2 = get_longest_word(input_str, allow_hyphen=True)
 
-print(f"Самое длинное слово: '{r1[0]}' ({r1[1]})")
-print(f"Самое длинное слово (allowHyphen=True): '{r2[0]}' ({r2[1]})")
+print(f"Самое длинное слово: '{result1[0]}' ({result1[1]})")
+print(f"Самое длинное слово (allow_hyphen=True): '{result2[0]}' ({result2[1]})")

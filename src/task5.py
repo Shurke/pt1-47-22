5243a8e26e4acae6e887a861831fc5e265df357c
AlexShkal <leshashkal@gmail.com>
2022-02-16 23:05:05 +0300

    task5

--- src/task5.py
+++ src/task5.py
@@ -3,16 +3,16 @@
 циклические операторы и условные операторы. n - вводится
 """
 
-fib1 = 1
-fib2 = 1
+fibf = 1
+fibs = 1
 
 n = input("Номер элемента ряда: ")
 n = int(n)
 
 i = 0
 while i < n - 2:
-    fib_sum = fib1 + fib2
-    fib1 = fib2
-    fib2 = fib_sum
+    fib_sum = fibf + fibs
+    fibf = fibs
+    fibs = fib_sum
     i = i + 1
-print("Значение  элемента:", fib2)
+print("Значение  элемента:", fibs)

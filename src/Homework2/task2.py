24b06b5b6479916b1302735297a0ee1b4a57911e
RusherBLR <rusherblr@gmail.com>
2022-02-16 18:22:21 +0300

    task2

--- src/Homework2/task2.py
+++ src/Homework2/task2.py
@@ -11,14 +11,14 @@
 import copy
 
 lis = [x + y for x in "ab" for y in "bcd"]
-print(lis)
+print("Задание 1:\n", lis)
 lis = lis[::2]
-print(lis)
+print("Задание 2:\n", lis)
 new_lis = [x + y for x in "1234" for y in "a"]
-print(new_lis)
-print(new_lis.pop(1))
+print("Задание 3:\n", new_lis)
+print("Задание 4:\n", new_lis.pop(1))
 print(new_lis)
 lis_cop = copy.deepcopy(new_lis)
 lis_cop.insert(1, "2a")
-print(lis_cop)
+print("Задание 5:\n", lis_cop)
 print(new_lis)

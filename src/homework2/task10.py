deb405bc635177ca8e81dcac36dab4cb194bd745
Kissloroid <kissloroid@gmail.com>
2022-02-17 18:03:00 +0300

    HW 2, ten tasks

--- src/homework2/task10.py
+++ src/homework2/task10.py
@@ -6,3 +6,14 @@
 """
 
 
+input_list = [int(elmt) for elmt in input('Введите числа через пробел: ').split()]
+summ_nmbr = 0
+null_summ = 0
+while null_summ + summ_nmbr != len(input_list):
+    total = null_summ + summ_nmbr
+    if input_list[total] != 0:
+        input_list.insert(summ_nmbr, input_list.pop(total))
+        summ_nmbr += 1
+    else:
+        null_summ += 1
+print(input_list)

bf45ffe8a0b28a9f50fcb13e0574d57580d34ef3
Mekhadiuk <1mekhadziuk@gmail.com>
2022-02-22 01:23:26 +0300

    flake8 fixes

--- src/hw_1/task6.py
+++ src/hw_1/task6.py
@@ -9,11 +9,13 @@ def palindrom(num):
     first_num = num
     rev_num = 0
 
+
     while num > 0:
         last_num = num % 10
         rev_num = rev_num * 10 + last_num
         num = num // 10
     return rev_num == first_num
+
 
 input_num = int(input("Введите число: "))
 print(palindrom(input_num))

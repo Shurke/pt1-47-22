84da26df8bd8e1367762c6dcbf30a49f300f2dbb
Mekhadiuk <1mekhadziuk@gmail.com>
2022-02-22 03:09:52 +0300

    flake8 task6 fix

--- src/hw_1/task6.py
+++ src/hw_1/task6.py
@@ -9,27 +9,27 @@
 
 num = int(input('Введите число: '))
 check_sum = 1
-amount_of_num = 1
+amount_of_sum = 1
 correct_check = 0
 
 while check_sum > 0:
-    if num // (10 ** amount_of_num):
-        amount_of_num += 1
+    if num // (10 ** amount_of_sum):
+        amount_of_sum += 1
     else:
         check_sum = 0
 
-if amount_of_num % 2 == 0:
-    num_of_check = int(amount_of_num / 2)
+if amount_of_sum % 2 == 0:
+    num_of_check = int(amount_of_sum /2)
 else:
-    num_of_check = int((amount_of_num - 1) / 2)
+    num_of_check = int((amount_of_sum - 1) / 2)
 
-for check in range(1, num_of_check + 1):
-    left = ((num // (10 ** (amount_of_num - check))) % 10)
+for check in range (1, num_of_check + 1):
+    left = ((num // (10 ** (amount_of_sum - check))) % 10)
     right = (num % (10 ** check)) // (10 ** (check - 1))
 
-if left == right:
+    if left == right:
         correct_check += 1
-if check == correct_check:
-    print(f'Число {num} является палиндромом')
-else:
-    print(f'Число {num} не является палиндромом')
+    if check == correct_check:
+        print(f'Число {num} является палиндромом')
+    else:
+        print(f'Число {num} не является палиндромом')

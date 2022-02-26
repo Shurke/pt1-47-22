6b337ec578c419a6ef82d475bd78dab04764a073
TatsianaSauko <miljanzowa@mail.ru>
2022-02-26 17:34:02 +0300

    add task10

--- src/homework3/task10.py
+++ src/homework3/task10.py
@@ -5,7 +5,7 @@
 
 number_1 = int(input("Введите первое число: "))
 number_2 = int(input("Введите второе число: "))
-while number_1 != 0 and number_2 != 0:
+while all([number_1, number_2]):
     if number_1 > number_2:
         number_1 = number_1 % number_2
     else:

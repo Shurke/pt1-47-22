a75f65248ba8cbc5befec7adc9ba7020f61ab1b8
DmitryBakacheuski <98980557+DmitryBakacheuski@users.noreply.github.com>
2022-03-18 20:23:29 +0300

    task4

--- src/task4.py
+++ src/task4.py
@@ -1,3 +1,19 @@
 """Написать программу которая находит ближайшую степень двойки к введенному числу. 10(8), 20(16),
  1(1), 13(16)
 """
+
+
+number = int(input('Введиче число для нахождения ближайшей степени двойки: '))
+last_result = 1
+
+
+while number >= 2**last_result:
+    previous = last_result
+    last_result += 1
+
+if number == 1:
+    print('Ближайшую степень двойки к введенному числу', number)
+elif number - (2**previous) > (2**last_result) - number:
+    print('Ближайшую степень двойки к введенному числу', (2**last_result))
+else:
+    print('Ближайшую степень двойки к введенному числу', (2**previous))

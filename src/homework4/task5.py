b4bb79e43e60320eebd6bb5f0a0d3d1a05789aa0
Jenderlion <vladchesyan@gmail.com>
2022-03-18 23:49:53 +0300

    task5.py added: nearest divisor

---
+++ src/homework4/task5.py
@@ -0,0 +1,24 @@
+"""
+Вводится число найти его максимальный делитель, являющийся степенью двойки. 10(2) 16(16), 12(4).
+"""
+
+
+def nearest_divisor(inp_num=9):
+    start_degree = 1
+    degree_list = [1]
+
+    while degree_list[-1] < inp_num:
+        degree_list.append(2 ** start_degree)
+        start_degree += 1
+
+    for divisor in degree_list[::-1]:
+        if inp_num % divisor == 0:
+            break
+    if divisor is None:
+        return 'Делителя нет (как так?)'
+    else:
+        return f'{inp_num}({divisor})'
+
+
+for i in [10, 16, 12]:
+    print(nearest_divisor(i))

00bf46b00b04703ec2984ec21d618471d60049a0
Jenderlion <vladchesyan@gmail.com>
2022-03-19 23:10:49 +0300

    task14.py added: raise error

---
+++ src/homework4/task14.py
@@ -0,0 +1,32 @@
+"""
+Создайте декоратор, который вызывает задекорированную функцию пока она не выполнится без исключений
+(но не более n раз - параметр декоратора). Если превышено количество попыток, должно быть возбуждено
+исключение типа TooManyErrors.
+Т.е. если во время работы функции возникло какое-то исключение, то функция должна перезапуститься
+еще раз. Количество перезапусков ограничено параметром декоратора.
+"""
+
+
+def decorator_with_limit(func, limit=10):
+
+    def wrapper(*args, **kwargs):
+
+        num_of_try = 0
+        exit_flag = False
+        while exit_flag == False and num_of_try < limit:
+            try:
+                func(*args, **kwargs)
+                exit_flag = True
+            except Exception:
+                num_of_try += 1
+
+        if exit_flag == True:
+            print(f'Function executed after {num_of_try + 1} try.')
+        else:
+            raise BufferError('TooManyErrors')
+
+    return wrapper
+
+
+decorator_with_limit(sum, 5)([3, 4])
+decorator_with_limit(sum, 5)(['frd', 4])

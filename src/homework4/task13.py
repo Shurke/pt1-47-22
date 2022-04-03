56a64fdb3b21e360b7f6f53f07665cd4cc33ad89
Jenderlion <vladchesyan@gmail.com>
2022-04-03 20:59:19 +0300

    task13: functions naming and documentation changes, added main() func

--- src/homework4/task13.py
+++ src/homework4/task13.py
@@ -19,54 +19,49 @@ elements.txt.
 """
 
 
-def word_with_elem(word: str, sequence: list) -> str:
-    """Trying to make a /silicon/ from elements in /sequence/
+def main():
+    def word_with_elem(word: str, sequence: list) -> str:
+        """Trying to make a /silicon/ from elements in /sequence/
 
-    Return SiLiCoN or empty string if this is not possible.
-    """
-    try_list = [False for x in range(0, len(word) + 1)]
-    try_list[0] = []
+        Return SiLiCoN or empty string if this is not possible.
 
-    for i in range(1, len(word) + 1):
+        :param word: input word to select elements (Silicon -> SiLiCoNe)
+        :param sequence: the sequence from which the word will be composed.
+        :return: Composed word (like a SiLiCoNe)
+        """
+        try_list = [False for x in range(0, len(word) + 1)]
+        try_list[0] = []
+        for i in range(1, len(word) + 1):
+            variants = []
+            for n in range(max(i - 3, 0), i):
+                if try_list[n] is False:  # variability
+                    continue
+                potential_element = word[n:i].title()  # for search in seq
+                if potential_element in sequence:
+                    variants.append(try_list[n] + [potential_element])
+            if variants:
+                try_list[i] = min(variants, key=len)
+        if try_list[-1] is False:
+            result = ''
+        else:
+            result = "".join(try_list[-1])
 
-        variants = []
+        return result
 
-        for n in range(max(i - 3, 0), i):
+    def get_chem_elements_from_file(file_name='elements.txt') -> dict:
+        """Return generated dict from file.
 
-            if try_list[n] is False:  # variability
-                continue
-            potential_element = word[n:i].title()  # for search in seq
+        :param file_name: database name, default = elements.txt
+        :return: dict in form {Short_name: Full_name}
+        """
+        with open(file_name, 'r') as opened_file:
+            elements_native = opened_file.readlines()
+            elements = {}
+            for string in elements_native:
+                string_list = string.split(',')
+                elements[string_list[1]] = string_list[2][:-1]
+        return elements
 
-            if potential_element in sequence:
-                variants.append(try_list[n] + [potential_element])
-
-        if variants:
-            try_list[i] = min(variants, key=len)
-
-    if try_list[-1] is False:
-        result = ''
-    else:
-        result = "".join(try_list[-1])
-
-    return result
-
-
-def get_sequence_dict_from_file(file_name='elements.txt') -> dict:
-    """Return dict with sequence {Short_name: Full_name} from file.
-
-    Default file = elements.txt
-    """
-    with open(file_name, 'r') as opened_file:
-        elements_native = opened_file.readlines()
-        elements = {}
-        for string in elements_native:
-            string_list = string.split(',')
-            elements[string_list[1]] = string_list[2][:-1]
-        elements_native.clear()
-    return elements
-
-
-if __name__ == '__main__':
     while True:
         what_to_do = input('Please, type word or help: ')
         if what_to_do == 'help':
@@ -75,7 +70,7 @@ if __name__ == '__main__':
                   'p_table \t--> to see results for all elements in period table\n'
                   'exit \t\t--> to close the program')
         elif what_to_do == 'p_table':
-            seq_dict = get_sequence_dict_from_file()
+            seq_dict = get_chem_elements_from_file()
             for value in seq_dict.values():
                 received_word = word_with_elem(value, list(seq_dict.keys()))
                 if received_word != '':
@@ -85,5 +80,9 @@ if __name__ == '__main__':
             exit('Closed by user')
 
         else:
-            seq = list(get_sequence_dict_from_file().keys())
+            seq = list(get_chem_elements_from_file().keys())
             print(f'Result: {word_with_elem(what_to_do, seq)}')
+
+
+if __name__ == '__main__':
+    main()

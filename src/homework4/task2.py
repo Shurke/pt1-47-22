6fe40a13b74ddccd589c9615ddfcb741b35ec5e5
Jenderlion <vladchesyan@gmail.com>
2022-04-03 21:23:05 +0300

    task2: documentation changes and removed blank lines

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -8,28 +8,26 @@
 
 
 def get_lists_of_ranges(input_list: list) -> str:
-    """Collapses list into string with its ranges"""
+    """Collapses list into string with its ranges
+
+    :param input_list: sorted list with numbers
+    :return: string with ranges in list
+    """
     range_list_native = [input_list[0]]
     result_list = []
-
     for elem_ind in range(1, len(input_list)):
-
         if input_list[elem_ind] - 1 == input_list[elem_ind - 1]:
             range_list_native.append(input_list[elem_ind])
         else:
             result_list.append(range_list_native)
             range_list_native = [input_list[elem_ind]]
     result_list.append(range_list_native)
-
     range_list = []
-
     for elem in result_list:
-
         if len(elem) == 1:
             range_list.append(str(elem[0]))
         else:
             range_list.append(f'{elem[0]}-{elem[-1]}')
-
     result = ','.join(range_list)
 
     return result

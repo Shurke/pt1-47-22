561fcc1eb22c46cf0cb69bf018da0578d9b44934
Jenderlion <vladchesyan@gmail.com>
2022-04-03 21:42:34 +0300

    task5: type annotation, main() and input added

--- src/homework4/task5.py
+++ src/homework4/task5.py
@@ -3,23 +3,37 @@
 """
 
 
-def nearest_divisor(inp_num=9) -> str:
-    """Return string with nearest (degree of 2) divisor like 'NUMBER(DIVISOR)'"""
-    start_degree = 1
-    degree_list = [1]
+def main():
+    def nearest_divisor(inp_num: int = 9) -> str:
+        """Return string with nearest (degree of 2) divisor like 'NUMBER(DIVISOR)'"""
+        start_degree = 1
+        degree_list = [1]
 
-    while degree_list[-1] < inp_num:
-        degree_list.append(2 ** start_degree)
-        start_degree += 1
+        while degree_list[-1] < inp_num:
+            degree_list.append(2 ** start_degree)
+            start_degree += 1
 
-    for divisor in degree_list[::-1]:
-        if inp_num % divisor == 0:
-            break
-    if divisor is None:
-        return 'Делителя нет (как так?)'
-    else:
-        return f'{inp_num}({divisor})'
+        for divisor in degree_list[::-1]:
+            if inp_num % divisor == 0:
+                break
+        if divisor is None:
+            return 'Делителя нет (как так?)'
+        else:
+            return f'{inp_num}({divisor})'
+
+    while True:
+        what_to_do = input('Please type number, demo or exit: ')
+
+        if what_to_do == 'exit':
+            exit('Closed by user')
+
+        elif what_to_do == 'demo':
+            for i in [10, 20, 1, 13]:
+                print(nearest_divisor(i))
+
+        else:
+            print(nearest_divisor(int(what_to_do)))
 
 
-for i in [10, 16, 12]:
-    print(nearest_divisor(i))
+if __name__ == '__main__':
+    main()

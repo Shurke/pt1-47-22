05fa7a05153390a870cf35a6d684eea9113b0d1c
Jenderlion <vladchesyan@gmail.com>
2022-04-03 21:31:02 +0300

    task3: added main(), condition for while: if ...:

--- src/homework4/task3.py
+++ src/homework4/task3.py
@@ -9,27 +9,28 @@
 """
 
 
-def binary_pyramid(m=1, n=4) -> str:
-    """Return binary pyramid in range from /m/ to /n/"""
-    pyramid_sum = 0
+def main():
+    def binary_pyramid(m: int = 1, n: int = 4) -> str:
+        """Return binary pyramid in range from /m/ to /n/"""
+        pyramid_sum = 0
 
-    for num in range(m, n + 1):
-        pyramid_sum += int(str(bin(num))[2:])
+        for num in range(m, n + 1):
+            pyramid_sum += int(str(bin(num))[2:])
 
-    return str(bin(pyramid_sum))[2:]
+        return str(bin(pyramid_sum))[2:]
+
+    while True:
+        what_to_do = input('Please type m & n separate by space or exit: ')
+        if what_to_do == 'exit':
+            exit('Closed by user')
+        else:
+            m__and_n = what_to_do.split()
+            if len(m__and_n) == 2 and m__and_n[0].isdigit() and m__and_n[1].isdigit():
+                m_int, n_int = int(m__and_n[0]), int(m__and_n[1])
+                print(binary_pyramid(m_int, n_int))
+            else:
+                print('Incorrect data')
 
 
 if __name__ == '__main__':
-    while True:
-        what_to_do = input('Please type m & n separate by space or exit: ')
-
-        if what_to_do == 'exit':
-            exit('Closed by user')
-
-        else:
-            try:
-                m__and_n = what_to_do.split()
-                m_int, n_int = int(m__and_n[0]), int(m__and_n[1])
-                print(binary_pyramid(m_int, n_int))
-            except IndexError:
-                print('Too little data')
+    main()

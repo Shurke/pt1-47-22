31b8020053c7ac3047ef02bde45b21dd96db4be4
Kir3103 <strunets96@gmail.com>
2022-04-08 15:58:56 +0300

    hw4

---
+++ src/homework4/task4.py
@@ -0,0 +1,26 @@
+"""
+Написать программу которая находит ближайшую степень двойки к введенному числу.
+10(8), 20(16), 1(1), 13(16)
+"""
+
+
+def two_degree(user_numb):
+    """
+    conversion to binary and then finding the nearest degree
+    :param user_numb: value from user
+    :return: nearest degree of two
+    """
+    binary = str((bin(user_numb)))[2:]
+
+    res_deg = 1
+    for deg in range(1, len(binary)):
+        if 2 ** deg >= res_deg and abs(user_numb - 2 ** deg) <= abs(user_numb - 2 ** (deg + 1)):
+            res_deg = 2 ** deg
+        elif 2 ** deg >= res_deg and abs(user_numb - 2 ** deg) >= abs(user_numb - 2 ** (deg + 1)):
+            res_deg = 2 ** (deg + 1)
+
+    return f'Ближайшая степень двойки к введенному числу: {res_deg}'
+
+
+numb = int(input('Введите число для получения ближайшей степени двойки: '))
+print(two_degree(numb))

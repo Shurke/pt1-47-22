8130818e69825b8480fda44a0846c9d87ab6d3f0
DmitryBakacheuski <98980557+DmitryBakacheuski@users.noreply.github.com>
2022-04-29 00:04:27 +0300

    task11

--- src/task11.py
+++ src/task11.py
@@ -17,3 +17,58 @@
 просто “1996”. Если пользователем на вход программы не передал никаких данных, то необходимо
 вывести все данные за все время.
 """
+
+import zipfile
+
+
+def top_10_names(year):
+    catallog_of_baby_names = zipfile.ZipFile("baby_names.zip", "r")
+    boys = catallog_of_baby_names.open(f"BabyNames/{int(year)}_BoysNames.txt")
+    girls = catallog_of_baby_names.open(f"BabyNames/{int(year)}_GirlsNames.txt")
+    list_boys = [name.decode().split(None, 1)[0] for name in boys]
+    list_girls = [name.decode().split(None, 1)[0] for name in girls]
+    boys.close()
+    girls.close()
+    boys_and_girls = [name for name in list_boys if name in list_girls]
+    top10_boys = []
+    top10_girls = []
+    counter_boys = 0
+    counter_girls = 0
+
+    while len(top10_boys) < 10:
+        if list_boys[counter_boys] not in boys_and_girls:
+            top10_boys.append(list_boys[counter_boys])
+        counter_boys += 1
+    while len(top10_girls) < 10:
+        if list_girls[counter_girls] not in boys_and_girls:
+            top10_girls.append(list_girls[counter_girls])
+        counter_girls += 1
+    print(f"Топ 10 имен для мальчиков в {year} году: {top10_boys}")
+    print(f"Топ 10 имен для девочек в {year} году: {top10_girls}")
+    if len(boys_and_girls) > 0:
+        print(f"Топ универсальных имен в {year} году: {boys_and_girls}")
+    else:
+        print(f"В {year} году не было универсальных")
+
+
+def years(year):
+    if year == "":
+        for num_of_year in range(1900, 2013):
+            top_10_names(num_of_year)
+    else:
+        year = year.split("-")
+        if len(year) == 2:
+            for num_of_year in range(int(year[0]), int(year[1])+1):
+                if num_of_year in range(1900, 2013):
+                    top_10_names(num_of_year)
+                else:
+                    print(f"О именах в {num_of_year} году нет данных")
+        elif len(year) == 1:
+            if int(year[0]) in range(1900, 2013):
+                top_10_names(year[0])
+            else:
+                print(f"О именах в {year[0]} году нет данных")
+
+
+input_year = input("Введите интересующей вас год: ")
+years(input_year)

e73e1e38fdbea9062f33acd20af8533ccbc2fec5
m4noon <thejonka@gmail.com>
2022-05-03 20:18:38 +0300

    Task 14

---
+++ src/homework4/task14.py
@@ -0,0 +1,32 @@
+"""
+Создайте декоратор, который вызывает задекорированную функцию пока она не выполнится без
+исключений (но не более n раз - параметр декоратора). Если превышено количество попыток,
+должно быть возбуждено исключение типа TooManyErrors. Т.е. если во время работы функции
+возникло какое-то исключение, то функция должна перезапуститься еще раз.
+Количество перезапусков ограничено параметром декоратора.
+"""
+
+
+class TooManyErrors(ValueError):
+    pass
+
+
+def decor_limit(limit):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for n in range(1, limit + 1):
+                try:
+                    return func(*args, **kwargs)
+                except ValueError:
+                    if n == limit:
+                        raise TooManyErrors("Превышено количество попыток")
+        return wrapper
+    return decorator
+
+
+@decor_limit(limit=5)
+def summary(a, b):
+    return a + b
+
+
+print(summary(5, 15))

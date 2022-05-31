48d13d6015f9d1c7dc95c3c9359c854d46cb4e4c
Kir3103 <strunets96@gmail.com>
2022-05-31 21:15:49 +0300

    test_task2

---
+++ src/homework5/test_task2.py
@@ -0,0 +1,48 @@
+"""Test module for task2 (RealString) tests"""
+import unittest
+import ddt
+import task2
+
+
+@ddt.ddt
+class TestRealString(unittest.TestCase):
+    """Test case for RealString"""
+
+    def setUp(self) -> None:
+        self.real_str = task2.RealString
+
+    @ddt.data(
+        ('meat', 'мясо', True),
+        ('car', 'машина', False)
+    )
+    @ddt.unpack
+    def test_str_eq(self, str_1, str_2, expect):
+        """Test case for compare equal strings {0} and {1}"""
+        str_to_compare = self.real_str(str_1).__eq__(str_2)
+        self.assertEqual(str_to_compare, expect)
+
+    @ddt.data(
+        ('ball', 'мяч', False),
+        ('car', 'машина', True),
+        ('food', 'meal', False)
+    )
+    @ddt.unpack
+    def test_str_less(self, str_1, str_2, expect):
+        """Test case for compare (less) strings {0} and {1}"""
+        str_to_compare_1 = self.real_str(str_1)
+        str_to_compare_2 = self.real_str(str_2)
+        result = str_to_compare_1 < str_to_compare_2
+        self.assertEqual(result, expect)
+
+    @ddt.data(
+        ('ball', 'мяч', True),
+        ('car', 'машина', False),
+        ('food', 'meal', False)
+    )
+    @ddt.unpack
+    def test_str_more(self, str_1, str_2, expect):
+        """Test case for compare (more) strings {0} and {1}"""
+        str_to_compare_1 = self.real_str(str_1)
+        str_to_compare_2 = self.real_str(str_2)
+        result = str_to_compare_1 > str_to_compare_2
+        self.assertEqual(result, expect)
